503a928641a43a5803ccadafed34551b1d7f368a
HarshPatelSchool <harshpatelschool2021@gmail.com>
2022-01-26 17:43:38 -0500

    Created Heuristic File

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,9 @@
 package src;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Game {
-
-    public static void main(String [ ] args) {
-
+    public static void main(String [] args) {
+        File boardTxt = new File(args[0]);
     }
 }
