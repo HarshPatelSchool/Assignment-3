40fb66e5256cd72b864db9bfb8708882b12916e5
Harsh Patel <harshpatelschool2021@gmail.com>
2022-01-29 20:23:45 -0500

    AStar Draft

--- src/AStar.java
+++ src/AStar.java
@@ -1,11 +1,14 @@
 package src;
 
+import java.util.TreeMap;
+
 public class AStar {
     private Coord G, S;
     private Heuristic h;
     private Board b;
     private Agent[][] scores;
     private int rows, columns;
+
     public AStar(Board b, Heuristic h){
         char[][] board = b.getBoard();
         rows = board.length;
@@ -20,14 +23,26 @@ public class AStar {
                     S = new Coord(x, y);
             }
         }
-        scores[G.getY()][G.getX()] = new Agent(b, G.getX(), G.getY(), Direction.UP);
-        run(G.getX(), G.getY());
+
+        scores[S.getY()][S.getX()] = new Agent(b, S.getX(), S.getY()); //Replace with .clone eventually
+        run(S.getX(), S.getY(), new Agent(b, S.getX(), S.getY()));
     }
 
-    private void run(int x, int y){
-        int[] directionScores = {0,0,0,0};
-        if(G.getX() + 1 < columns && G.getY() < rows);
+    private void run(int x, int y, Agent a) {
+        if(x != G.getX() && y!= G.getY()){
+            if(scores[x][y]==null || scores[x][y].getScore() < a.getScore()) {
+                TreeMap<Integer, Coord> directions = new TreeMap<>();
+                if (y + 1 < rows)
+                    directions.put(h.calculateHeuristic(x, y+1, G.getX(), G.getY()) + b.getVal(new Coord(x , y+1)), new Coord(x,y+1));
+                if (x + 1 < columns)
+                    directions.put(h.calculateHeuristic(x +1 , y, G.getX(), G.getY()) + b.getVal(new Coord(x+1, y)), new Coord(x+1, y));
+                if (y - 1 >= 0)
+                    directions.put(h.calculateHeuristic(x, y-1, G.getX(), G.getY()) + b.getVal(new Coord(x , y-1)), new Coord(x,y-1));
+                if (x - 1 >= 0)
+                    directions.put(h.calculateHeuristic(x -1 , y, G.getX(), G.getY()) + b.getVal(new Coord(x-1, y)), new Coord(x-1, y));
 
-
+            }
+        }
     }
+
 }
