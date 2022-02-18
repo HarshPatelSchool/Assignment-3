d7a9be3800affdf2900b69eaf5323abbdd2c14fe
Harsh Patel <harshpatelschool2021@gmail.com>
2022-02-18 15:11:48 -0500

    Added functionality to run algorithm on random board for 2 hours and turn it into one .csv file

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -12,22 +13,6 @@ public class Game {
     public static void main(String [] args) throws IOException, CloneNotSupportedException {
         File boardTxt = new File(args[0]); //Stores the file path from commandline argument
         int heuristicChoice = Integer.parseInt(args[1]); //Stores the heuristic being used (1-6) from the commandline argument
-        GenerateBoards.generateBoards(10,500,500); //Comment this line out to stop generating new random boards
-
-        Heuristic heuristic = new Heuristic(heuristicChoice); //Generates the heuristic method based on user choice
-        Board board = new Board(boardTxt); //Generates board from user's file
-        AStar search = new AStar(board, heuristic); //Searches through the board using the user-specified heuristic method
-        Agent goal = search.getBestGoal(); //Gets back the best Agent from Start to Goal
-        ArrayList<Agent> agentPath = goal.getAgentPath();
-        ArrayList<String> path = goal.getPath();
-
-        int cost = goal.getScore()-100;
-        System.out.println("Score = "+ goal.getScore());
-        System.out.println("Number of Actions for Optimal Path = "+goal.getPath().size());
-        System.out.println("Nodes Visited for Optimal Path = "+goal.getNodes());
-        System.out.println("Nodes Expanded to find Optimal Path= "+search.getVisited().size());
-
-
         PrintWriter csv = new PrintWriter("src/test.csv");
         StringBuffer sb = new StringBuffer();
         sb.append("Action Taken");
@@ -42,6 +27,23 @@ public class Game {
         sb.append(',');
         sb.append("Score to Goal");
         sb.append('\n');
+        long startTime = System.currentTimeMillis();
+        while(System.currentTimeMillis()-startTime<1000){ //7200000ms = 2hr
+            GenerateBoards.generateBoards(1,10,10); //Comment this line out to stop generating new random boards
+            Heuristic heuristic = new Heuristic(heuristicChoice); //Generates the heuristic method based on user choice
+            Board board = new Board(boardTxt); //Generates board from user's file
+            runSimulation(board, heuristic, sb);
+        }
+        csv.write(sb.toString());
+        csv.close();
+    }
+
+    private static void runSimulation(Board board, Heuristic heuristic, StringBuffer sb) throws IOException, CloneNotSupportedException {
+        AStar search = new AStar(board, heuristic); //Searches through the board using the user-specified heuristic method
+        Agent goal = search.getBestGoal(); //Gets back the best Agent from Start to Goal
+        ArrayList<Agent> agentPath = goal.getAgentPath();
+        ArrayList<String> path = goal.getPath();
+        int cost = goal.getScore()-100;
         for(int i = 0; i<agentPath.size(); i++){
             int total = 0;
             Agent a = agentPath.get(i);
@@ -61,8 +63,12 @@ public class Game {
             sb.append(a.getScore()-cost);
             sb.append('\n');
         }
-        csv.write(sb.toString());
-        csv.close();
+        /*
+        System.out.println("Score = "+ goal.getScore());
+        System.out.println("Number of Actions for Optimal Path = "+goal.getPath().size());
+        System.out.println("Nodes Visited for Optimal Path = "+goal.getNodes());
+        System.out.println("Nodes Expanded to find Optimal Path= "+search.getVisited().size());
+         */
 
     }
 }
